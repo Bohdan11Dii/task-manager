3ae523a74aa1994a84291978695c16329daf710e
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-07-18 12:29:09 +0300

    Change one line in model such as deadline in Task model

--- task/models.py
+++ task/models.py
@@ -1,8 +1,7 @@
-import datetime
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -40,35 +39,44 @@ class Worker(AbstractUser):
         ordering = ["username"]
 
     def __str__(self):
-        return f"{self.username} ({self.position.name})"
+        return self.username
 
 
 class Task(models.Model):
     PRIORITY = (
-        ("u", "Urgent"),
-        ("h", "High"),
-        ("m", "Medium"),
-        ("l", "Low")
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low")
     )
 
     name = models.CharField(max_length=255)
     description = models.TextField()
-    deadline = models.DateTimeField("Completion date", default=timezone.now())
-    is_completed = models.BooleanField()
-    priority = models.CharField(max_length=10, choices=PRIORITY, default="u")
+    deadline = models.DateTimeField("Completion date")
+    is_completed = models.BooleanField("Completed")
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY,
+        default="Urgent"
+    )
     task_type = models.ForeignKey(
         TaskType,
         on_delete=models.CASCADE,
         related_name="task"
     )
-    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
-
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
 
     class Meta:
         ordering = ["deadline"]
 
     def __str__(self):
-        return (f"{self.name} "
+        return (f"{self.name}"
                 f"(task_type: {self.task_type.name},"
                 f"completed: {self.is_completed})"
                 )
+
+    def get_absolute_url(self):
+        return reverse("task:task-detail", kwargs={"pk": self.pk})
