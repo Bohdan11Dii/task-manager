2a858d9f13818a80fc7e649dc942cd4a51992c59
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-07-31 13:41:54 +0300

    Delete all migration and run migrate to create one files migrations and create custom migrations file to run auto

---
+++ task/migrations/0002_auto_20230731_1007.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+from django.db.migrations import RunPython
+
+
+def func(apps, schema_editor):
+    from django.core.management import call_command
+    call_command('loaddata', 'fixture_data.json')
+
+
+def reverse_func(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("task", "0001_initial"),
+    ]
+
+    operations = [
+        RunPython(func, reverse_func),
+    ]
