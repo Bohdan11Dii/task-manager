7533dbaee3a8d2d88ab0e216c42faa1ca9624d86
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-07-31 22:36:02 +0300

    Change DJANGO_DEBUG to DEBUG

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.environ.get("DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "it-task-manager-99on.onrender.com",]
 
